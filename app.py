530fd8c3cbe32c2f63da73c1c90c050610d8ba8f
mishra27 <mishra27@wisc.edu>
2019-07-08 14:52:18 -0500

    Code and a video to track points

---
+++ app.py
@@ -0,0 +1,95 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('a.mp4')
+
+
+# params for ShiTomasi corner detection
+feature_params = dict(maxCorners=100,
+                      qualityLevel=0.3,
+                      minDistance=7,
+                      blockSize=7)
+
+# Parameters for lucas kanade optical flow
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=2,
+                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# Create some random colors
+color = np.random.randint(0, 255, (100, 3))
+
+# Take first frame and find corners in it
+ret, old_frame = cap.read()
+
+
+def draw_circle(event, x, y, flags, param):
+    global mouseX, mouseY
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img, (x, y), 10, (255, 0, 0), -1)
+        mouseX, mouseY = x, y
+
+
+img = old_frame
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_circle)
+
+while(1):
+    cv2.imshow('image', img)
+    k = cv2.waitKey(20) & 0xFF
+    if k == 27:
+        break
+    elif k == ord('s'):
+        print(mouseX, mouseY)
+        cv2.destroyAllWindows()
+        break
+
+im = old_frame
+height, width, depth = im.shape
+
+circle_img = np.zeros((height, width), np.uint8)
+cv2.circle(circle_img, (mouseX, mouseY), 50, 1, thickness=-1)
+
+masked_data = cv2.bitwise_and(im, im, mask=circle_img)
+
+# cv2.imshow("masked", masked_data)
+# cv2.waitKey(0)
+
+
+old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+p0 = cv2.goodFeaturesToTrack(old_gray, mask=circle_img, **feature_params)
+
+# Create a mask image for drawing purposes
+mask = np.zeros_like(old_frame)
+
+
+while(1):
+    ret, frame = cap.read()
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # calculate optical flow
+    p1, st, err = cv2.calcOpticalFlowPyrLK(
+        old_gray, frame_gray, p0, None, **lk_params)
+
+    # Select good points
+    good_new = p1[st == 1]
+    good_old = p0[st == 1]
+
+    # draw the tracks
+    for i, (new, old) in enumerate(zip(good_new, good_old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+        frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+    img = cv2.add(frame, mask)
+
+    cv2.imshow('frame', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+    # Now update the previous frame and previous points
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1, 1, 2)
+
+cv2.destroyAllWindows()
+cap.release()
